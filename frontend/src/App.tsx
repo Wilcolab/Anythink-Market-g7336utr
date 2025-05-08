d6856783b6964323810230e0c12d71f26a42b6d8
azimzimmer <126082719+azimzimmer@users.noreply.github.com>
2025-05-08 06:45:17 +0000

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <h1 className="App-title">DriveNets Dashboard</h1>
+          <h1 className="App-title">DriveNets Dashboard </h1>
         </div>
       </header>
       <main className="App-main">
